4b466bbdbcbfc33ccc5edbe800b844380f97cab4
ur <wongtakshing37@gmail.com>
2024-02-23 23:52:00 +0800

    from orin

--- src/ros2_ws/build/pioneer_msgs/ament_cmake_python/pioneer_msgs/pioneer_msgs/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from pioneer_msgs.msg._motion_command import MotionCommand  # noqa: F401
-from pioneer_msgs.msg._status import Status  # noqa: F401
--- src/ros2_ws/build/pioneer_msgs/colcon_command_prefix_test.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/src/ros2_ws/install/pioneer_msgs/share/pioneer_msgs/package.sh"
