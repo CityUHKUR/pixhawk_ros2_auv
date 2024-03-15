5f6ff3433e996512fc2fae0a1406f51c16e92f77
desms11 <121095759+desms11@users.noreply.github.com>
2024-03-15 23:37:55 +0800

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -2,5 +2,7 @@
 echo "source /opt/ros/humble/local_setup.bash" >> ~/.bashrc
 echo "source /src/ros2_ws/install/setup.bash" >> ~/.bashrc
 # ros2 run imu_publisher publisher
+source /opt/ros/humble/local_setup.bash
+source /src/ros2_ws/install/setup.bash
 ros2 run pixpubsub pixhawk_worker
 /bin/bash
